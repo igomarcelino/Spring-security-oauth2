e31538835734fa1c3cf925ab9700a61805988b0a
Igo Marcelino <geslucian@hotmail.com>
2024-12-20 19:40:46 -0300

    Cria o endpoint para criar um novo usuario, habilita o EnableMethodSecurity em SegurityConfig para habilitar o PreAuthorize nos endpoints dos controllers

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,6 +1,6 @@
 INSERT INTO TB_USER(user_login, user_password)VALUES('admin', '$2a$12$C0vwo.6NB7Pzb9uv4c5QzOFntMQKm3lKgBBrbTN.AH9TCHIwVrF7W');
 
-INSERT INTO TB_ROLES(role_name)VALUES('admin');
-INSERT INTO TB_ROLES(role_name)VALUES('user');
+INSERT INTO TB_ROLES(role_name)VALUES('ADMIN');
+INSERT INTO TB_ROLES(role_name)VALUES('BASIC');
 
 INSERT INTO TB_USER_ROLES(user_id, role_id)VALUES(1,1);
