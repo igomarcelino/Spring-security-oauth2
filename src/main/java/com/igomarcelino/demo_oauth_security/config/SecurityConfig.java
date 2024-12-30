6b6610c68a0a6f23a87483a3a723c69d1c4d85d4
Igo Marcelino <geslucian@hotmail.com>
2024-12-30 10:32:43 -0300

    feat: adiciona o delete tweet validando o usuario pelo jwt

--- src/main/java/com/igomarcelino/demo_oauth_security/config/SecurityConfig.java
+++ src/main/java/com/igomarcelino/demo_oauth_security/config/SecurityConfig.java
@@ -5,16 +5,25 @@ import com.nimbusds.jose.jwk.JWKSet;
 import com.nimbusds.jose.jwk.RSAKey;
 import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.jwt.*;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
@@ -36,11 +45,15 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
 
-        return httpSecurity.authorizeHttpRequests(authorize ->
+        return httpSecurity.
+
+                authorizeHttpRequests(authorize ->
                         authorize.
+                                requestMatchers("/h2-console/**").permitAll().
                                 requestMatchers("/login/**").permitAll(). // permite a rota de login
                                 requestMatchers("/newuser/**").permitAll(). // permite a rota para criacao de novo usuario
-                                anyRequest().authenticated()).csrf(csrf -> csrf.disable()).
+                                anyRequest().authenticated()).csrf(CsrfConfigurer::disable).// disabilita o csrf
+                headers((headers) -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin)). // para poder utilizar o h2-console spring security 6
                 oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults())).
                 sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)).
                 build();
@@ -64,6 +77,8 @@ public class SecurityConfig {
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+
 }
 
 
--- src/main/java/com/igomarcelino/demo_oauth_security/controller/TweetController.java
+++ src/main/java/com/igomarcelino/demo_oauth_security/controller/TweetController.java
@@ -1,15 +1,18 @@
 package com.igomarcelino.demo_oauth_security.controller;
 
 import com.igomarcelino.demo_oauth_security.dto.CreateTweetDTO;
+import com.igomarcelino.demo_oauth_security.entities.Roles;
 import com.igomarcelino.demo_oauth_security.entities.Tweet;
 import com.igomarcelino.demo_oauth_security.repository.TweetRepository;
 import com.igomarcelino.demo_oauth_security.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.crossstore.ChangeSetPersister;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 public class TweetController {
@@ -28,6 +31,21 @@ public class TweetController {
         tweet.setUser(user.get());
         tweet.setContent(createTweetDTO.content());
         tweetRepository.save(tweet);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping(value = "/tweet/{id}")
+    public ResponseEntity<Void> deleteTweetById(@PathVariable("id") Integer id, JwtAuthenticationToken token){
+            var tweet = tweetRepository.findById(id).
+                    orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        System.out.println("Tweet not found");
+            if (tweet.getUser().getId().toString().equals(token.getName()) || tweet.getUser().getId() == 1) {
+                tweetRepository.deleteById(tweet.getId());
+            }else {
+                throw new ResponseStatusException(HttpStatus.FORBIDDEN);
+            }
+
+
         return ResponseEntity.ok().build();
     }
 }
